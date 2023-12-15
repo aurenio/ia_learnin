810aad06ea10cecfb0eab2329a78f0ea8f5ae5bb
aurenio <aurenio1@gmail.com>
2023-12-15 00:16:54 -0300

    corrigindo estimador, exemplo de ia nao adequada demo_four

--- ia_demo_four.py
+++ ia_demo_four.py
@@ -1,66 +1,75 @@
 from sklearn.svm import LinearSVC
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-#tentativa de previsão de projeto finalizado de acordo com um historico em csv 
-
 
 url = "https://gist.githubusercontent.com/guilhermesilveira/1b7d5475863c15f484ac495bd70975cf/raw/16aff7a0aee67e7c100a2a48b676a2d2d142f646/projects.csv"
-
 url_info = pd.read_csv(url)
 
-
 rename_info = {
-    "unfinished" : "nao_terminados",
-    "expected_hours" : "espectativa_horas",
+    "unfinished" : "nao_comprado",
+    "expected_hours" : "horas",
     "price" : "valor"
 }
 
 url_info = url_info.rename(columns=rename_info)
+
 
 troca = {
     0 : 1,
     1 : 0
 }
 
+url_info["comprado"] = url_info["nao_comprado"].map(troca)
 
-url_info["finalizados"] = url_info.nao_terminados.map(troca)
-
-
-sns.scatterplot(x = "espectativa_horas", y = "valor", data = url_info, hue = "finalizados")
-
-plt.savefig("grafico.png")
-
-
-zero_one = sns.FacetGrid(data=url_info,hue="finalizados", col="finalizados")
-zero_one.map(sns.scatterplot, "espectativa_horas", "valor" )
-plt.savefig("comparacao")
-
-
-sucess = url_info[["finalizados"]]
-
-unsucess = url_info[["nao_terminados"]]
-
-data = url_info[["espectativa_horas", "valor"]]
-
+info_x = url_info[["horas", "valor"]]
+info_y = url_info[["comprado"]]
 seed = 20
+treino_x, teste_x, treino_y, teste_y = train_test_split(info_x, info_y, test_size=0.25, random_state=seed, stratify=info_y)
 
-treino_x, teste_x, treino_y, teste_y = train_test_split(data, sucess, test_size=0.25, random_state=seed, stratify=sucess)
+estudo = LinearSVC(random_state=seed)
+estudo.fit(treino_x, treino_y)
+chute_bot = estudo.predict(teste_x)
+chute_comparacao = np.ones(540)
 
-model = LinearSVC()
+acerto_bot = accuracy_score(chute_bot, teste_y)
+acerto_comparacao = accuracy_score(chute_comparacao, teste_y)
 
-model.fit(treino_x, treino_y)
+print(f"O bot acertou {acerto_bot} e tem que superar {acerto_comparacao}")
 
-analis = model.predict(teste_x)
+x_min = teste_x.horas.min()
+x_max = teste_x.horas.max()
+y_min = teste_x.valor.min()
+y_max = teste_x.valor.max()
 
-print(accuracy_score(analis, teste_y))
+pixel = 100
+eixo_x = np.arange(x_min, x_max, (x_max - x_min) / pixel)
+eixo_y = np.arange(y_min, y_max, (y_max - y_min) / pixel)
+xx, yy = np.meshgrid(eixo_x, eixo_y)
+pontos = np.c_[xx.ravel(), yy.ravel()]
 
-previ_base = np.ones(540)
+z = estudo.predict(pontos)
+z = z.reshape(xx.shape)
+print(z)
 
-chute = accuracy_score(analis, previ_base)
+sns.scatterplot(y="valor", x="horas",  hue=teste_y.values.flatten(), data=teste_x)
+plt.savefig("estudo_3")
 
-print(f"meta a superar {chute}")
+
+plt.scatter(teste_x.horas, teste_x.valor, c=teste_y.values.flatten(), s=1)
+plt.contourf(xx, yy, z, alpha=0.2)
+plt.savefig("mygod.png")
+
+sns.scatterplot(x="horas", y="valor", data=url_info, hue="comprado")
+plt.savefig("prova_1.png")
+
+double = sns.FacetGrid(data=url_info, hue="comprado", col="comprado")
+double.map(sns.scatterplot, "horas", "valor")
+plt.savefig("prova_2_tentativa.png")
+
+
+
